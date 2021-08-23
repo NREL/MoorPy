Reducing redundant tension code, rotation fix, Line.getCoordinate:
- It looks like some blocks of code were duplicated (with slight edits)
  when the capabilities for tension-colored plots were added. Only the
  first block will run, so the new capabilities are not realized.
- - Removed duplicate code in Line.getCoords.
- - Other instances still need to be addressed.
- Added a Line.getCoordinate method which will return the position and
  tension for a single point along a Line's arc length.
- Removed a double-counting of rotation in System.transform: rotation
  of a Body. Now, only the relative point attachment locations on a
  body are rotated. This corrects errors in rotated system fairleads.

moorpy/line.py
@@ -155,8 +155,10 @@ class Line():
         
         
 
-    def getLineCoords(self, Time):    # formerly UpdateLine
+    def getLineCoords(self, Time, n=0):    # formerly UpdateLine
         '''Gets the updated line coordinates for drawing and plotting purposes.'''
+    
+        if n==0: n = self.nNodes
     
         # if a quasi-static analysis, just call the catenary function to return the line coordinates
         if self.qs==1:
@@ -174,66 +176,7 @@ class Line():
         
             try:
                 (fAH, fAV, fBH, fBV, info) = catenary(LH, LV, self.L, self.sys.lineTypes[self.type].EA, 
-                                                  self.sys.lineTypes[self.type].w, self.cb, HF0=self.HF, VF0=self.VF, nNodes=self.nNodes, plots=1) 
-            except CatenaryError as error:
-                raise LineError(self.number, error.message)
-            
-            Xs = self.rA[0] + info["X"]*dr[0]/LH
-            Ys = self.rA[1] + info["X"]*dr[1]/LH
-            Zs = self.rA[2] + info["Z"]
-            
-            return Xs, Ys, Zs
-            
-        # otherwise, count on read-in time-series data
-        else:
-            # figure out what time step to use
-            ts = self.getTimestep(Time)
-            
-            # drawing rods
-            if self.isRod > 0:
-            
-                k1 = np.array([ self.xp[ts,-1]-self.xp[ts,0], self.yp[ts,-1]-self.yp[ts,0], self.zp[ts,-1]-self.zp[ts,0] ]) / self.length # unit vector
-                
-                k = np.array(k1) # make copy
-            
-                Rmat = np.array(rotationMatrix(0, np.arctan2(np.hypot(k[0],k[1]), k[2]), np.arctan2(k[1],k[0])))  # <<< should fix this up at some point, MattLib func may be wrong
-                
-                # make points for appropriately sized cylinder
-                d = self.sys.lineTypes[self.type].d
-                Xs, Ys, Zs = makeTower(self.length, np.array([d, d]))
-                
-                # translate and rotate into proper position for Rod
-                coords = np.vstack([Xs, Ys, Zs])
-                newcoords = np.matmul(Rmat,coords)
-                Xs = newcoords[0,:] + self.xp[ts,0]
-                Ys = newcoords[1,:] + self.yp[ts,0]
-                Zs = newcoords[2,:] + self.zp[ts,0]
-                
-                return Xs, Ys, Zs
-                
-            # drawing lines
-            else:
-                
-                return self.xp[ts,:], self.yp[ts,:], self.zp[ts,:]
-        '''Gets the updated line coordinates for drawing and plotting purposes.'''
-    
-        # if a quasi-static analysis, just call the catenary function to return the line coordinates
-        if self.qs==1:
-        
-            depth = self.sys.depth
-        
-            dr =  self.rB - self.rA                 
-            LH = np.hypot(dr[0], dr[1])     # horizontal spacing of line ends
-            LV = dr[2]                      # vertical offset from end A to end B
-            
-            if np.min([self.rA[2],self.rB[2]]) > -depth:
-                self.cb = -depth - np.min([self.rA[2],self.rB[2]])   # if this line's lower end is off the seabed, set cb negative and to the distance off the seabed
-            elif self.cb < 0:   # if a line end is at the seabed, but the cb is still set negative to indicate off the seabed
-                self.cb = 0.0     # set to zero so that the line includes seabed interaction.
-        
-            try:
-                (fAH, fAV, fBH, fBV, info) = catenary(LH, LV, self.L, self.sys.lineTypes[self.type].EA, 
-                                                  self.sys.lineTypes[self.type].w, self.cb, HF0=self.HF, VF0=self.VF, nNodes=self.nNodes, plots=1) 
+                                                  self.sys.lineTypes[self.type].w, self.cb, HF0=self.HF, VF0=self.VF, nNodes=n, plots=1) 
             except CatenaryError as error:
                 raise LineError(self.number, error.message)
             
@@ -268,13 +211,31 @@ class Line():
                 Ys = newcoords[1,:] + self.yp[ts,0]
                 Zs = newcoords[2,:] + self.zp[ts,0]
                 
-                return Xs, Ys, Zs
+                return Xs, Ys, Zs, None
                 
             # drawing lines
             else:
                 
                 return self.xp[ts,:], self.yp[ts,:], self.zp[ts,:]
+    
+    
+    def getCoordinate(self, s, n=100):
+        '''Returns position and tension at a specific point along the line's unstretched length'''
+        
+        dr =  self.rB - self.rA                 
+        LH = np.hypot(dr[0], dr[1])  
             
+        Ss = np.linspace(0, self.L, n)
+        Xs, Ys, Zs, Ts = self.getLineCoords(0.0, n=n)
+        
+        X = np.interp(s, Ss, Xs)*dr[0]/LH
+        Y = np.interp(s, Ss, Ys)*dr[1]/LH
+        Z = np.interp(s, Ss, Zs)
+        T = np.interp(s, Ss, Ts)
+        
+        return X, Y, Z, T
+        
+    
     
     def drawLine2d(self, Time, ax, color="k", Xuvec=[1,0,0], Yuvec=[0,0,1]):
         '''Draw the line on 2D plot (ax must be 2D)
@@ -303,7 +264,7 @@ class Line():
         
         if self.isRod > 0:
             
-            Xs, Ys, Zs = self.getLineCoords(Time)
+            Xs, Ys, Zs, Te = self.getLineCoords(Time)
         
             # apply any 3D to 2D transformation here to provide desired viewing angle
             Xs2d = Xs*Xuvec[0] + Ys*Xuvec[1] + Zs*Xuvec[2] 
@@ -317,7 +278,7 @@ class Line():
         # drawing lines...
         else:
         
-            Xs, Ys, Zs = self.getLineCoords(Time)
+            Xs, Ys, Zs, Te = self.getLineCoords(Time)
             
             # apply any 3D to 2D transformation here to provide desired viewing angle
             Xs2d = Xs*Xuvec[0] + Ys*Xuvec[1] + Zs*Xuvec[2] 
@@ -357,7 +318,7 @@ class Line():
     
         if self.isRod > 0:
             
-            Xs, Ys, Zs = self.getLineCoords(Time)
+            Xs, Ys, Zs, Ts = self.getLineCoords(Time)
             
             for i in range(int(len(Xs)/2-1)):
                 linebit.append(ax.plot(Xs[2*i:2*i+2],Ys[2*i:2*i+2],Zs[2*i:2*i+2]            , color=color))  # side edges
@@ -370,7 +331,7 @@ class Line():
         # drawing lines...
         else:
         
-            Xs, Ys, Zs = self.getLineCoords(Time)
+            Xs, Ys, Zs, Ts = self.getLineCoords(Time)
             
             linebit.append(ax.plot(Xs, Ys, Zs, color=color))
             
@@ -462,7 +423,7 @@ class Line():
         
         if self.isRod > 0:
             
-            Xs, Ys, Zs = self.getLineCoords(Time)
+            Xs, Ys, Zs, Ts = self.getLineCoords(Time)
             
             for i in range(int(len(Xs)/2-1)):
                         
@@ -476,7 +437,7 @@ class Line():
         # drawing lines...
         else:
         
-            Xs, Ys, Zs = self.getLineCoords(Time)
+            Xs, Ys, Zs, Ts = self.getLineCoords(Time)
             linebit[0][0].set_data(Xs,Ys)    # (x and y coordinates)
             linebit[0][0].set_3d_properties(Zs)         # (z coordinates)               
                 
@@ -727,7 +688,7 @@ class Line():
 
             
         if self.isRod > 0:
-            Xs, Ys, Zs = self.getLineCoords(Time)
+            Xs, Ys, Zs, Ts = self.getLineCoords(Time)
             Ts = self.getLineTens(Time)
             
             for i in range(int(len(Xs)/2-1)):
@@ -742,7 +703,7 @@ class Line():
         # drawing lines...
         else:
            
-            Xs, Ys, Zs = self.getLineCoords(Time)
+            Xs, Ys, Zs, Ts = self.getLineCoords(Time)
             Ts = self.getLineTens(Time)
             for i in range(0,len(Xs)-1):
                 r = ((Ts[i] +Ts[i+1])/2 - mint)/(maxt - mint)

moorpy/system.py
@@ -131,7 +131,7 @@ class System():
         #print("Created Point "+str(self.pointList[-1].number))
         # handle display message if/when MoorPy is reorganized by classes
         
-    def addLine(self, lUnstr, type_string, nSegs=20, pointA=0, pointB=0, cb=0):
+    def addLine(self, lUnstr, type_string, nSegs=40, pointA=0, pointB=0, cb=0):
         '''Convenience function to add a Line to a mooring system
 
         Parameters
@@ -1181,7 +1181,8 @@ class System():
 
         def transform6(X):
             Xrot = np.matmul(rotMat, X[:3]*scale)   
-            X = np.hstack([Xrot + tVec, X[3], X[4], X[5]+rot*np.pi/180.0])
+            #X = np.hstack([Xrot + tVec, X[3], X[4], X[5]+rot*np.pi/180.0])  # this line would be to also rotate the body, but that's double counting if we also rotate the fairlead positions
+            X = np.hstack([Xrot + tVec, X[3], X[4], X[5]])
             return X
 
         # update positions of all objects