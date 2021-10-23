98f8bb28447cc40f1e4cf5e700281dfe30e5220b
Matt Hall <5151457+mattEhall@users.noreply.github.com>
2021-10-22 22:51:08 -0600

    Adding examples folder with 2 basic demos.

---
+++ examples/imported_system.py
@@ -0,0 +1,36 @@
+# MoorPy Example Script:
+# Example of manually setting up a mooring system in MoorPy and solving equilibrium.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import moorpy as mp
+from moorpy.MoorProps import getLineProps
+
+
+
+# ----- set up the mooring system and floating body -----
+
+# Create new MoorPy System and set its depth
+ms = mp.System(file='sample.txt')
+
+# ----- run the model to demonstrate -----
+
+ms.initialize()                                             # make sure everything's connected
+
+# find equilibrium of just the mooring lines (the body is 'coupled' and will by default not be moved)
+ms.solveEquilibrium()                                       # equilibrate
+fig, ax = ms.plot()                                         # plot the system in original configuration
+
+# add hydrostatic stiffness properties to the body, then add a load and see how it responds
+ms.bodyList[0].rM=np.array([0,0,100])                       #
+ms.bodyList[0].AWP=1e3
+ms.bodyList[0].f6Ext = np.array([3e6, 0, 0, 0, 0, 0])       # apply an external force on the body 
+ms.solveEquilibrium3(DOFtype='both')                        # equilibrate (specify 'both' to enable both free and coupled DOFs)
+fig, ax = ms.plot(ax=ax, color='red')                       # plot the system in displaced configuration (on the same plot, in red)
+
+print(f"Body offset position is {ms.bodyList[0].r6}")
+        
+plt.show()
+
+
+
